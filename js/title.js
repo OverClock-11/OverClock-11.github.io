0ccd4aa0a0f9a2f819e97600e15ade3618ec1382
OverClock-11 <zjh2020@hnu.edu.cn>
2023-06-01 15:36:43 +0800

    Site updated: 2023-06-01 15:36:42

---
+++ js/title.js
@@ -0,0 +1,1 @@
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="(つД`)不再看看嘛？",clearTimeout(titleTime)):(document.title="(*ˊ•ω•ˋ)欢迎光临~",titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));
